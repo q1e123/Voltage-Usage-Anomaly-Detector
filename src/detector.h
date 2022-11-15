2707ab131924e11f1d92c1ab552b1febf78a802d
Robert <robertnicolaesolca@gmail.com>
2022-11-15 08:53:57 +0200

    added data type + modified train header

--- src/detector.h
+++ src/detector.h
@@ -5,14 +5,14 @@ struct Model{
 };
 
 struct Point{
-
+    double voltage;
 };
 
 bool predict(Model model, Point point){
 
 }
 
-Model train(List train_data){
+Model train(Point* train_data, size_t train_data_size){
 
 }
 
