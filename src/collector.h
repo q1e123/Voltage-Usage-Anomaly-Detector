a2717f48e07b9d41f88b9ac369a77fea3f5919a9
Tohaneanu Andrei-Dumitru <tohaneanu_andrei7@yahoo.com>
2023-01-15 17:23:27 +0200

    translate

--- src/collector.h
+++ src/collector.h
@@ -1,17 +1,17 @@
 double get_voltage(){
     
     
-    //variables for measured voltage and calculated current
+    //variabile pentru masurarea voltilor si calcularea curentului
     double Vout = 0;
     double Current = 0;
 
-    //constants for scale factor
-    const double scale_factor = 0.1; //20A
+   
+    const double scale_factor = 0.1; 
 
-    //Constants for A/D converter resolution
-    //10-bit ADC, so 1024 possible values
-    //reference voltage is 5V
-    //zero point is half of reference voltage
+    //Constante pentru rezolutia convertorului A/D
+    //10-bit ADC, deci 1024 posibile valori
+    //voltajul de referinta: 5V
+    //zero point e jumatate din voltajul de referinta
 
     const double vRef = 5.00;
     const double resConvert = 1024;
@@ -19,18 +19,18 @@ double get_voltage(){
     double zeroPoint = vRef/2;
 
     
-        //Vout is read 1000 times for precision
+        //Vout e citit de 1000 de ori, pentru precizie
         for (int i=0;i<1000;i++){
             Vout = (Vout + (resADC*analogRead(A0)));
             delay(1);
         }
-        //Get Vout in mV
+        //Conversie in mV
         Vout=Vout/1000;
 
-        //Convert Vout into Current using scale Factor
+        
         Current=(Vout - zeroPoint)/scale_factor;
 
-        //print Vout and Current to two Current 
+        //Printare
         Serial.print("Vout= ");
         Serial.print(Vout,2);
         Serial.print(" Volts");
